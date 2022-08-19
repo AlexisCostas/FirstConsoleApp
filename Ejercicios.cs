aae1d815f3aadf354d189c01aa40ef71e949b771
AlexisCostas <109992064+AlexisCostas@users.noreply.github.com>
2022-08-19 09:36:34 -0300

    Add files via upload
    Testing uploading a file 2

---
+++ Ejercicios.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mostrar_los_pares
+{
+    class Ejercicios
+    {
+        /// <summary>
+        /// Te pide ingresar 2 numeros y te dice cuales son pares, en caso de ninguno ser par, tambien lo notifica por pantalla
+        /// </summary>
+        public void Ejercicio1()
+        {
+            Console.WriteLine("Vas a ingresar 2 numeros, luego te mostraremos aquellos pares");
+            Console.WriteLine("Toca cualquier tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Ingresa el primer numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            bool a = true;
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("El primer numero (" + num1 + ") es par");
+            }
+            else
+            {
+                a = false;
+            }
+            if (num2 % 2 == 0)
+            {
+                Console.WriteLine("El segundo numero (" + num2 + ") es par");
+            }
+            else if (!a)
+            {
+                Console.WriteLine("Ninguno de los números es par");
+            }
+        }
+
+        /// <summary>
+        /// Programa que imprime los numeros perfectos entre 1 y "n", a "n" lo debemos ingresar por teclado
+        /// </summary>
+
+        public void Ejercicio2()
+        {
+
+            Console.WriteLine("Programa que imprime los numeros perfectos entre 1 y n");
+            Console.WriteLine("Ingrese \"n\" ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Los numeros perfectos entre 1 y " + n + " son: ");
+            for (int i = 1; i <= n; i++)
+            {
+
+                int total = 0;
+
+                for (int j = (i - 1); j > 0; j--)
+                {
+                    if (i % j == 0)
+                    {
+                        total += j;
+                    }
+
+                }
+                if (total == i)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+        /// <summary>
+        /// Programa que te pide que ingreses el nombre del admin
+        /// /// </summary>
+        public void Ejercicio3()
+        {
+            string nombre = "";
+            while (nombre != "alexis")
+            {
+                Console.WriteLine("Ingresa el nombre del admin");
+                nombre = Console.ReadLine();
+                if (nombre != "alexis")
+                {
+                    Console.WriteLine(nombre + " no es el nombre del admin");
+                }
+            }
+            Console.WriteLine("Felicidades! ha ingresado el nombre del admin");
+
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mostrar_los_pares;
+using System.Globalization;
+Ejercicios ej = new Ejercicios();
+int opcionMenu = 0;
+do
+{
+    Console.WriteLine("Elegi una opcion ");
+    Console.WriteLine("1 - Numeros pares ");
+    Console.WriteLine("2 - Numeros perfectos ");
+    Console.WriteLine("3 - Probar el nombre de admin");
+    Console.WriteLine("0 - Salir de la app");
+    opcionMenu = Convert.ToInt32(Console.ReadLine());
+    switch (opcionMenu)
+    {
+        case 1:
+            ej.Ejercicio1();
+            break;
+        case 2:
+            ej.Ejercicio2();
+            break;
+        case 3:
+            ej.Ejercicio3();
+            break;
+    }
+
+} while (opcionMenu != 0);
